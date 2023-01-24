2c34c9c68eb7b275d75d492ce8ddb94a01a31df4
jonataslaet <jonataslaetprogramador@gmail.com>
2023-01-23 22:03:25 -0300

    Projeto iniciado
    Criado um controller de cumprimentos HelloController

---
+++ backend-java/src/main/java/com/github/jonataslaet/laetfood/LaetfoodApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaetfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaetfoodApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/github/jonataslaet/laetfood/LaetfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaetfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
